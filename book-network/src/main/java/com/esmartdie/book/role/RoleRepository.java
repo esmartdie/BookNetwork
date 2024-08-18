433203e170f36da3e48836080124b7aaabae98f8
Diego Martins <esmartdie@gmail.com>
2024-08-18 22:25:35 +0200

    user and role repository

---
+++ book-network/src/main/java/com/esmartdie/book/role/RoleRepository.java
@@ -0,0 +1,6 @@
+package com.esmartdie.book.role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+}
---
+++ book-network/src/main/java/com/esmartdie/book/user/UserRepository.java
@@ -0,0 +1,11 @@
+package com.esmartdie.book.user;
+
+import com.esmartdie.book.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
+}
