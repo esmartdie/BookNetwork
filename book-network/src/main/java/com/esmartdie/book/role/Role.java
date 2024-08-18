3380cf831b901511b60468fd9cd248410c7c2fa8
Diego Martins <esmartdie@gmail.com>
2024-08-18 20:38:24 +0200

    initial commit, user and role entities

---
+++ book-network/src/main/java/com/esmartdie/book/role/Role.java
@@ -0,0 +1,31 @@
+package com.esmartdie.book.role;
+
+import com.esmartdie.book.user.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Role {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @Column(unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    @JsonIgnore
+    private List<User> users;
+}
