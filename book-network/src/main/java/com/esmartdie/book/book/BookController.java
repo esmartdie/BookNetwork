f3d720a288059b3942c0ed62912f137f67db220f
Diego Martins <esmartdie@gmail.com>
2024-08-25 17:16:36 +0200

    findBookByID and FindAllBooks controllers

--- book-network/src/main/java/com/esmartdie/book/book/BookController.java
+++ book-network/src/main/java/com/esmartdie/book/book/BookController.java
@@ -1,14 +1,16 @@
 package com.esmartdie.book.book;
 
+import com.esmartdie.book.common.PageResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.naming.ldap.PagedResultsControl;
+import java.util.List;
 
 @RestController
 @RequestMapping("books")
@@ -24,5 +26,21 @@ public class BookController {
             Authentication connectedUser
     ){
         return ResponseEntity.ok(service.save(request, connectedUser));
+    }
+
+    @GetMapping
+    public ResponseEntity<BookResponse> findBookById(
+            @PathVariable("book-id") Integer bookId
+    ){
+        return ResponseEntity.ok(service.findById(bookId));
+    }
+
+    @GetMapping
+    public ResponseEntity<PageResponse<BookResponse>> findAllBooks(
+            @RequestParam (name = "page", defaultValue="0", required = false) int page,
+            @RequestParam (name = "page", defaultValue="0", required = false) int size,
+            Authentication connectedUser
+    ){
+        return ResponseEntity.ok(service.findAllBooks(page, size, connectedUser));
     }
 }
--- book-network/src/main/java/com/esmartdie/book/book/BookService.java
+++ book-network/src/main/java/com/esmartdie/book/book/BookService.java
@@ -1,9 +1,18 @@
 package com.esmartdie.book.book;
 
+import com.esmartdie.book.common.PageResponse;
 import com.esmartdie.book.user.User;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -16,5 +25,29 @@ public class BookService {
         Book book = bookMapper.toBook(request);
         book.setOwner(user);
         return bookRepository.save(book).getId();
+    }
+
+    public BookResponse findById(Integer bookId) {
+        return bookRepository.findById(bookId)
+                .map(bookMapper::toBookResponse)
+                .orElseThrow(()-> new EntityNotFoundException("No book found with the id:: " + bookId));
+    }
+
+    public PageResponse<BookResponse> findAllBooks(int page, int size, Authentication connectedUser) {
+        User user = ((User) connectedUser.getPrincipal());
+        Pageable pageable = PageRequest.of(page, size, Sort.by("createdDate").descending());
+        Page <Book> books = bookRepository.findAllDisplayableBooks(pageable, user.getId());
+        List<BookResponse> bookResponseList = books.stream()
+                .map(bookMapper::toBookResponse)
+                .toList();
+        return new PageResponse<>(
+                bookResponseList,
+                books.getNumber(),
+                books.getSize(),
+                books.getTotalElements(),
+                books.getTotalPages(),
+                books.isFirst(),
+                books.isLast()
+        );
     }
 }
