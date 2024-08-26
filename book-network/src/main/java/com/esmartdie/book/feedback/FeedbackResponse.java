b6ea2ba7258a839807e8966ebec6743278af231a
Diego Martins <esmartdie@gmail.com>
2024-08-26 23:20:59 +0200

    Feedback feature

---
+++ book-network/src/main/java/com/esmartdie/book/feedback/FeedbackResponse.java
@@ -0,0 +1,17 @@
+package com.esmartdie.book.feedback;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class FeedbackResponse {
+
+    private Double note;
+    private String comment;
+    private boolean ownFeedback;
+}
